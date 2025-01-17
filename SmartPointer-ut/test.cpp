e82752512869dbb14a388780dcb448dbf6f7db23
Paweł Ziemann <paweljar.ziemann@gmail.com>
2025-01-17 19:31:39 +0100

    Added tests + a few methods

--- SmartPointer-ut/test.cpp
+++ SmartPointer-ut/test.cpp
@@ -1,7 +1,81 @@
+#include "SmartPointer.h"
 #include "gtest/gtest.h"
 
-TEST(TestCaseName, TestName)
+TEST(UniquePtrTests, ShouldReturnPointerValueWhenDereferenceOperatorIsCalled)
 {
-    EXPECT_EQ(1, 1);
-    EXPECT_TRUE(true);
+    UniquePointer<int> uniquePointer(new int(5));
+    int value = *(uniquePointer);
+    EXPECT_EQ(value, 5);
+}
+
+TEST(UniquePtrTests, ShouldReturnPointerWhenReleaseIsCalled)
+{
+    UniquePointer<int> uniquePointer(new int(5));
+    auto pointer = uniquePointer.release();
+    EXPECT_EQ(*(pointer), 5);
+    delete pointer;
+}
+
+TEST(UniquePtrTests, ShouldReturnNullptrWhenResetIsCalled)
+{
+    UniquePointer<int> uniquePointer(new int(5));
+    uniquePointer.reset();
+    auto pointer = uniquePointer.get();
+    EXPECT_EQ(pointer, nullptr);
+}
+
+TEST(SharedPtrTests, SharedPointersShouldReturnTheSameValueWhenDereferenceOperatorCalled)
+{
+    SharedPointer<int> sharedPointer1(new int(5));
+    SharedPointer<int> sharedPointer2(sharedPointer1);
+    EXPECT_EQ(*(sharedPointer1), *(sharedPointer2));
+    EXPECT_EQ(sharedPointer1, sharedPointer2);
+}
+
+TEST(SharedPtrTests, CallingResetOnSharePointer1ShouldNotAffectSharedPointer2)
+{
+    SharedPointer<int> sharedPointer1(new int(5));
+    SharedPointer<int> sharedPointer2(sharedPointer1);
+    sharedPointer1.reset();
+    EXPECT_EQ(sharedPointer1.get(), nullptr);
+    EXPECT_EQ(*sharedPointer2, 5);
+}
+
+TEST(SharedPtrTests, WhenUniqueCalledShouldReturnTrueAndWhenANewSharedPointerIsCreatedShouldReturnFalse)
+{
+    SharedPointer<int> sharedPointer1(new int(5));
+    bool isUnique1 = sharedPointer1.unique();
+    SharedPointer<int> sharedPointer2(sharedPointer1);
+    bool isUnique2 = sharedPointer1.unique();
+    EXPECT_EQ(isUnique1, true);
+    EXPECT_EQ(isUnique2, false);
+}
+
+TEST(SharedPtrTests, VerifySharedPointerReferenceCountAfterCopyConstruction)
+{
+    SharedPointer<int> sharedPointer1(new int(5));
+    SharedPointer<int> sharedPointer2(sharedPointer1);
+    EXPECT_EQ(sharedPointer1.use_count(), 2);
+}
+
+TEST(SharedPtrTests, VerifySharedPointerReferenceCountAfterCopyConstruction2)
+{
+    SharedPointer<int> sharedPointer1(new int(5));
+    {
+        EXPECT_EQ(sharedPointer1.use_count(), 1);
+        SharedPointer<int> sharedPointer2(sharedPointer1);
+        EXPECT_EQ(sharedPointer1.use_count(), 2);
+    }
+    EXPECT_EQ(sharedPointer1.use_count(), 1);
+    sharedPointer1.reset();
+    EXPECT_EQ(sharedPointer1.use_count(), 0);
+}
+
+TEST(SharedPtrTests, VerifySharedPointerReferenceCountAfterCopyConstruction3)
+{
+    SharedPointer<int> sharedPointer1(nullptr);
+    SharedPointer<int> sharedPointer2(sharedPointer1);
+
+    EXPECT_EQ(sharedPointer1.use_count(), 0);
+    EXPECT_EQ(sharedPointer2.use_count(), 0);
 }
--- SmartPointer.h
+++ SmartPointer.h
@@ -1,15 +1,17 @@
 #pragma once
+#include <cassert>
+#include <cstdint>
 
-template <typename T> class SmartPointer
+template <typename T> class UniquePointer
 {
   public:
-    SmartPointer(T *pointer) : pointer_(pointer)
+    UniquePointer(T *pointer) : pointer_(pointer)
     {
     }
 
-    SmartPointer(const SmartPointer &) = delete;
+    UniquePointer(const UniquePointer<T> &) = delete;
 
-    ~SmartPointer()
+    ~UniquePointer()
     {
         delete pointer_;
     }
@@ -24,6 +26,11 @@ template <typename T> class SmartPointer
         return pointer_;
     }
 
+    bool operator==(std::nullptr_t) const
+    {
+        return pointer_ == nullptr;
+    }
+
     T *release()
     {
         auto pointer = pointer_;
@@ -33,10 +40,108 @@ template <typename T> class SmartPointer
 
     void reset(T *pointer = nullptr)
     {
+        assert(pointer != pointer_);
         delete pointer_;
         pointer_ = pointer;
     }
 
+    T *get() const
+    {
+        return pointer_;
+    }
+
   private:
-    T *pointer_;
+    T *pointer_ = nullptr;
+};
+
+class ControlBlock
+{
+  public:
+    uint64_t counter_ = 0;
+};
+
+template <typename T> class SharedPointer
+{
+  public:
+    SharedPointer(T *pointer) : pointer_(pointer)
+    {
+        if (pointer_ != nullptr)
+        {
+            controlBlock_ = new ControlBlock();
+            controlBlock_->counter_++;
+        }
+    }
+
+    SharedPointer(const SharedPointer<T> &copyPointer)
+    {
+        pointer_ = copyPointer.pointer_;
+        controlBlock_ = copyPointer.controlBlock_;
+        if (pointer_ != nullptr)
+            controlBlock_->counter_++;
+    }
+
+    ~SharedPointer()
+    {
+        if (unique())
+        {
+            delete pointer_;
+            delete controlBlock_;
+        }
+        else if (controlBlock_ != nullptr)
+            controlBlock_->counter_--;
+    }
+
+    T &operator*()
+    {
+        return *pointer_;
+    }
+
+    T *operator->()
+    {
+        return pointer_;
+    }
+
+    bool operator==(const SharedPointer<T> &comparePtr) const
+    {
+        return pointer_ == comparePtr.get();
+    }
+
+    void reset(T *pointer = nullptr)
+    {
+        if (unique())
+        {
+            delete pointer_;
+            controlBlock_->counter_--;
+        }
+        else
+        {
+            if (controlBlock_ != nullptr)
+                controlBlock_->counter_--;
+            controlBlock_ = new ControlBlock();
+        }
+        pointer_ = pointer;
+    }
+
+    bool unique() const
+    {
+        if (controlBlock_ != nullptr)
+            return controlBlock_->counter_ == 1;
+        return false;
+    }
+
+    T *get() const
+    {
+        return pointer_;
+    }
+
+    uint64_t use_count() const
+    {
+        if (controlBlock_ != nullptr)
+            return controlBlock_->counter_;
+        return 0;
+    }
+
+  private:
+    ControlBlock *controlBlock_ = nullptr;
+    T *pointer_ = nullptr;
 };
